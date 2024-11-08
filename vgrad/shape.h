edb83d550e0ffbe8433392af8d77e66dc3a5b5d3
Alex Chen <axchen7@outlook.com>
2024-11-08 02:18:25 -0500

    Improve shape

--- vgrad/shape.h
+++ vgrad/shape.h
@@ -6,21 +6,36 @@ concept IsDimension = requires {
     { T::value } -> std::same_as<const int&>;
 };
 
+template <typename T>
+concept IsShape = requires {
+    { T::is_shape } -> std::same_as<const bool&>;
+} && T::is_shape;
+
 template <int V>
 struct Dimension {
     static constexpr int value = V;
 };
 
-struct BaseShape {};
-
-template <typename T>
-concept IsShape = std::is_base_of_v<BaseShape, T>;
+struct BaseShape {
+    static constexpr bool is_shape = true;
+};
 
 template <typename Outer, typename Inner>
 struct Shape {};
 
 template <IsDimension Outer, IsShape Inner>
-struct Shape<Outer, Inner> : BaseShape {
+struct Shape<Outer, Inner> {
+    static constexpr bool is_shape = true;
+
     static constexpr Outer outer;
     static constexpr Inner inner;
+
+    template <int I>
+    static constexpr auto at() {
+        if constexpr (I == 0) {
+            return outer;
+        } else {
+            return inner.template at<I - 1>();
+        }
+    }
 };
