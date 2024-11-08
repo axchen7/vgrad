424c7be4c0adcee829206e9136d3d7d688ba33e5
Alex Chen <axchen7@outlook.com>
2024-11-08 10:52:21 -0500

    Use named size/index type

--- vgrad/shape.h
+++ vgrad/shape.h
@@ -3,9 +3,12 @@
 
 namespace vgrad {
 
+using Size = unsigned int;
+using Index = int;  // allow negative indexing
+
 template <typename T>
 concept IsDimension = requires {
-    { T::value } -> std::same_as<const int&>;
+    { T::value } -> std::same_as<const Size&>;
 };
 
 template <typename T>
@@ -13,9 +16,9 @@ concept IsShape = requires {
     { T::is_shape } -> std::same_as<const bool&>;
 } && T::is_shape;
 
-template <int V>
+template <Size V>
 struct Dimension {
-    static constexpr int value = V;
+    static constexpr Size value = V;
 };
 
 template <typename Outer, typename Inner>
@@ -29,12 +32,12 @@ class Shape<Outer, Inner> {
     static constexpr Outer outer;
     static constexpr Inner inner;
 
-    static constexpr int rank = 1 + Inner::rank;
-    static constexpr int flat_size = Inner::rank == 0 ? Outer::value : Outer::value * Inner::flat_size;
+    static constexpr Size rank = 1 + Inner::rank;
+    static constexpr Size flat_size = Inner::rank == 0 ? Outer::value : Outer::value * Inner::flat_size;
 
-    template <int I>
+    template <Index I>
     static constexpr auto at() {
-        constexpr int i = normalize_index<I>();
+        constexpr auto i = normalize_index<I>();
         if constexpr (i == 0) {
             return outer;
         } else {
@@ -42,9 +45,9 @@ class Shape<Outer, Inner> {
         }
     }
 
-    template <int I>
+    template <Index I>
     static constexpr auto squeeze() {
-        constexpr int i = normalize_index<I>();
+        constexpr auto i = normalize_index<I>();
         if constexpr (i == 0) {
             return inner;
         } else {
@@ -52,12 +55,12 @@ class Shape<Outer, Inner> {
         }
     }
 
-    template <int I, IsDimension D>
+    template <Index I, IsDimension D>
     static constexpr auto unsqueeze(D dim) {
         if constexpr (I == rank) {
             return Shape<Outer, Shape<Inner, D>>{};
         } else {
-            constexpr int i = normalize_index<I>();
+            constexpr auto i = normalize_index<I>();
             if constexpr (i == 0) {
                 return Shape<D, Shape<Outer, Inner>>{};
             } else {
@@ -66,10 +69,10 @@ class Shape<Outer, Inner> {
         }
     }
 
-    template <int I1, int I2>
+    template <Index I1, Index I2>
     static constexpr auto transpose() {
-        constexpr int i1 = normalize_index<I1>();
-        constexpr int i2 = normalize_index<I2>();
+        constexpr auto i1 = normalize_index<I1>();
+        constexpr auto i2 = normalize_index<I2>();
         auto d1 = at<i1>();
         auto d2 = at<i2>();
         return Shape<Outer, Inner>{}
@@ -80,10 +83,10 @@ class Shape<Outer, Inner> {
     }
 
    private:
-    template <int I>
-    static constexpr int normalize_index() {
+    template <Index I>
+    static constexpr Size normalize_index() {
         if constexpr (I < 0) {
-            constexpr int i = rank + I;
+            constexpr auto i = rank + I;
             static_assert(i >= 0 && i < rank, "Invalid index");
             return i;
         } else {
@@ -95,8 +98,8 @@ class Shape<Outer, Inner> {
 
 struct EmptyShape {
     static constexpr bool is_shape = true;
-    static constexpr int rank = 0;
-    static constexpr int flat_size = 0;
+    static constexpr Size rank = 0;
+    static constexpr Size flat_size = 0;
 };
 
 constexpr auto make_shape() { return EmptyShape{}; }
