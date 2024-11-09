106f233dc9f593cf5e257cde9f252dac1db9290f
Alex Chen <axchen7@outlook.com>
2024-11-09 01:15:15 -0500

    Add transpose and reduce

--- vgrad/ops.h
+++ vgrad/ops.h
@@ -1,6 +1,8 @@
 #ifndef VGRAD_OPS_H_
 #define VGRAD_OPS_H_
 
+#include <span>
+
 #include "tensor.h"
 
 namespace vgrad {
@@ -11,7 +13,7 @@ concept TensorBinaryOpCompatible = TensorDTypeCompatible<A, B> && TensorShapeCom
 template <IsTensor A>
 auto unary_op(const A& a, auto op) {
     Tensor<typename A::Shape, typename A::DType> result;
-    for (Size i = 0; i < a.data_->size(); ++i) {
+    for (Size i = 0; i < a.data_->size(); i++) {
         (*result.data_)[i] = op((*a.data_)[i]);
     }
     return result;
@@ -21,8 +23,51 @@ template <IsTensor A, IsTensor B>
     requires TensorBinaryOpCompatible<A, B>
 auto binary_op(const A& a, const B& b, auto op) {
     Tensor<typename A::Shape, typename A::DType> result;
-    for (Size i = 0; i < a.data_->size(); ++i) {
+    for (Size i = 0; i < a.data_->size(); i++) {
         (*result.data_)[i] = op((*a.data_)[i], (*b.data_)[i]);
+    }
+    return result;
+}
+
+template <IsTensor A, Index I1, Index I2>
+auto transpose(const A& a) {
+    using NewShape = typename A::Shape::template Transpose<I1, I2>;
+    Tensor<NewShape, typename A::DType> result;
+
+    constexpr auto original_strides = A::Shape::strides();
+    constexpr auto new_strides = NewShape::strides();
+
+    for (Size i = 0; i < a.data_->size(); i++) {
+        std::array<Size, A::Shape::rank> indices;
+
+        Size orig_idx = i;
+        for (Size j = 0; j < indices.size(); j++) {
+            indices[j] = orig_idx / original_strides[j];
+            orig_idx %= original_strides[j];
+        }
+        std::swap(indices[I1], indices[I2]);
+
+        Size new_idx = 0;
+        for (Size j = 0; j < indices.size(); j++) {
+            new_idx += indices[j] * new_strides[j];
+        }
+
+        (*result.data_)[new_idx] = (*a.data_)[i];
+    }
+
+    return result;
+}
+
+template <IsTensor A>
+auto reduce_last_dim(const A& a, auto op) {
+    using NewShape = typename A::Shape::template Remove<-1>;
+    Tensor<NewShape, typename A::DType> result;
+
+    constexpr auto last_dim = A::Shape::flat_size / NewShape::flat_size;
+
+    for (Size i = 0; i < NewShape::flat_size; i++) {
+        std::span<const typename A::DType> slice{a.data_->begin() + i * last_dim, last_dim};
+        (*result.data_)[i] = op(slice);
     }
     return result;
 }
