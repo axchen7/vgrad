ea873d4b0a514e61658d684fcdb0a0f0f638f32c
Alex Chen <axchen7@outlook.com>
2024-11-20 13:40:53 -0500

    Remove typehint

--- vgrad/mnist.cpp
+++ vgrad/mnist.cpp
@@ -1,10 +1,9 @@
-#include "vgtensor.h"
-
 #include <tuple>
 
 #include "create_tensor.h"
 #include "ops.h"
 #include "optimizers.h"
+#include "vgtensor.h"
 
 using namespace vgrad;
 
@@ -26,9 +25,9 @@ template <IsDimension In, IsDimension Out, Number DType, IsDimension Inner>
 class Model {
    public:
     auto forward(auto x) const {
-        auto o1 = layer1.forward(x);   // typehint: [10000 x 128, float]
-        auto o2 = relu(o1);            // typehint: [10000 x 128, float]
-        auto o3 = layer2.forward(o2);  // typehint: [10000 x 10, float]
+        auto o1 = layer1.forward(x);
+        auto o2 = relu(o1);
+        auto o3 = layer2.forward(o2);
         return o3;
     }
 
@@ -44,15 +43,15 @@ class Model {
 };
 
 int main() {
-    using Inner = Dimension<128>;  // typehint: [128]
+    using Inner = Dimension<128>;
 
-    using TrainBatch = Dimension<10'000>;  // typehint: [10000]
-    using TestBatch = Dimension<1'000>;    // typehint: [1000]
+    using TrainBatch = Dimension<10'000>;
+    using TestBatch = Dimension<1'000>;
 
-    using ImgSize = Dimension<28>;                                // typehint: [28]
-    using FlatSize = Dimension<ImgSize::value * ImgSize::value>;  // typehint: [784]
+    using ImgSize = Dimension<28>;
+    using FlatSize = Dimension<ImgSize::value * ImgSize::value>;
 
-    using Classes = Dimension<10>;  // typehint: [10]
+    using Classes = Dimension<10>;
 
     auto train_imgs = import_vgtensor<float, MakeShape<TrainBatch, ImgSize, ImgSize>>("../torch/train_images.vgtensor");
     auto test_imgs = import_vgtensor<float, MakeShape<TestBatch, ImgSize, ImgSize>>("../torch/test_images.vgtensor");
@@ -60,19 +59,19 @@ int main() {
     auto train_labels = import_vgtensor<int32_t, MakeShape<TrainBatch>>("../torch/train_labels.vgtensor");
     auto test_labels = import_vgtensor<int32_t, MakeShape<TestBatch>>("../torch/test_labels.vgtensor");
 
-    auto train_flat = reshape<MakeShape<TrainBatch, FlatSize>>(train_imgs);  // typehint: [10000 x 784, float]
-    auto test_flat = reshape<MakeShape<TestBatch, FlatSize>>(test_imgs);     // typehint: [1000 x 784, float]
+    auto train_flat = reshape<MakeShape<TrainBatch, FlatSize>>(train_imgs);
+    auto test_flat = reshape<MakeShape<TestBatch, FlatSize>>(test_imgs);
 
     Model<FlatSize, Classes, float, Inner> model;
 
-    const float lr = 0.01;
-    const int epochs = 10;
+    const float lr = 0.1;
+    const int epochs = 100;
 
     optim::Adam optimizer{lr, model.params()};
 
     for (int i = 0; i < epochs; i++) {
-        auto out = model.forward(train_flat);          // typehint: [10000 x 10, float]
-        auto loss = cross_entropy(out, train_labels);  // typehint: [scalar, float]
+        auto out = model.forward(train_flat);
+        auto loss = cross_entropy(out, train_labels);
         optimizer.step(loss);
 
         std::cout << "Epoch " << i << " loss: " << loss.value() << std::endl;
