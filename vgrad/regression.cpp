3ebf58e26b625b69b94b2a7ae12695eed8809bd3
Alex Chen <axchen7@outlook.com>
2024-11-26 18:05:59 -0500

    Refactor linear model

--- vgrad/regression.cpp
+++ vgrad/regression.cpp
@@ -9,25 +9,35 @@
 using namespace vgrad;
 
 template <Number DType>
-class Model {
+class LinearModel {
    public:
-    Tensor<ScalarShape, DType> a = randn<DType, ScalarShape>();
-    Tensor<ScalarShape, DType> b = randn<DType, ScalarShape>();
-
     template <IsTensor X>
     auto forward(const X& x) const {
-        PROFILE_SCOPE("Model::forward");
-
+        PROFILE_SCOPE("LinearModel::forward");
         using Dim = typename X::Shape::template At<0>;
-
         auto range = arange<DType, Dim>();
-        auto error = x - (a * range + b);
-        auto loss = sum(pow(error, 2));
-        return loss;
+        return a * range + b;
     }
 
     auto params() { return make_params(a, b); }
+
+    template <typename T>
+    friend std::ostream& operator<<(std::ostream& os, const LinearModel<T>& model) {
+        os << model.a.value() << "x + " << model.b.value();
+        return os;
+    }
+
+   private:
+    Tensor<ScalarShape, DType> a = randn<DType, ScalarShape>();
+    Tensor<ScalarShape, DType> b = randn<DType, ScalarShape>();
 };
+
+auto loss(const auto& model, const auto& x) {
+    PROFILE_SCOPE("loss");
+    auto y = model.forward(x);
+    auto loss = sum(pow(x - y, 2));
+    return loss;
+}
 
 int main() {
     using Dim = Dimension<100>;
@@ -36,7 +46,7 @@ int main() {
     // auto x = zeros<DType, MakeShape<Dim>>();
     auto x = arange<DType, Dim>() + randn<DType, MakeShape<Dim>>() * 0.1;
 
-    Model<DType> model;
+    LinearModel<DType> model;
 
     const float lr = 0.1;
     const int epochs = 1000;
@@ -46,13 +56,13 @@ int main() {
     for (int epoch = 0; epoch < epochs; epoch++) {
         PROFILE_SCOPE("epoch");
 
-        auto loss = model.forward(x);
-        optimizer.step(loss);
+        auto l = loss(model, x);
+        optimizer.step(l);
 
         if (epoch % 20 == 0) {
-            std::cout << "Epoch " << epoch << "\tLoss: " << loss.value() << std::endl;
+            std::cout << "Epoch " << epoch << "\tLoss: " << l.value() << std::endl;
         }
     }
 
-    std::cout << "a: " << model.a.value() << "\tb: " << model.b.value() << std::endl;
+    std::cout << "Model: " << model << std::endl;
 }
