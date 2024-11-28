229e7f83d096de81fc3ebc640590e847de2c19e2
Alex Chen <axchen7@outlook.com>
2024-11-28 09:13:47 -0800

    Add PolynomialModel

--- vgrad/regression.cpp
+++ vgrad/regression.cpp
@@ -9,28 +9,48 @@
 using namespace vgrad;
 
 template <Number DType>
-class LinearModel {
+class ScalarModel {
    public:
-    template <IsTensor X>
-    auto forward(const X& x) const {
-        PROFILE_SCOPE("LinearModel::forward");
-        using Dim = typename X::Shape::template At<0>;
-        auto range = arange<DType, Dim>();
-        return a * range + b;
-    }
-
-    auto params() { return make_params(a, b); }
+    auto forward(const auto& x) const { return coeff; }
+    auto params() { return make_params(coeff); }
 
     template <typename T>
-    friend std::ostream& operator<<(std::ostream& os, const LinearModel<T>& model) {
-        os << model.a.value() << "x + " << model.b.value();
+    friend std::ostream& operator<<(std::ostream& os, const ScalarModel<T>& model) {
+        os << model.coeff;
         return os;
     }
 
    private:
-    Tensor<ScalarShape, DType> a = randn<DType, ScalarShape>();
-    Tensor<ScalarShape, DType> b = randn<DType, ScalarShape>();
+    Tensor<ScalarShape, DType> coeff = randn<DType, ScalarShape>();
 };
+
+template <Number DType, int degree>
+class PolynomialModel {
+   public:
+    template <IsTensor X>
+    auto forward(const X& x) const {
+        using Dim = typename X::Shape::template At<0>;
+        auto range = arange<DType, Dim>();
+        return coeff * pow(range, degree) + next.forward(x);
+    }
+
+    auto params() { return make_params(coeff, next); }
+
+    template <typename T>
+    friend std::ostream& operator<<(std::ostream& os, const PolynomialModel<T, degree>& model) {
+        os << model.coeff << "x^" << degree << " + " << model.next;
+        return os;
+    }
+
+   private:
+    Tensor<ScalarShape, DType> coeff = randn<DType, ScalarShape>();
+
+    using NextModel = std::conditional_t<degree - 1 == 0, ScalarModel<DType>, PolynomialModel<DType, degree - 1>>;
+    NextModel next;
+};
+
+template <Number DType>
+using LinearModel = PolynomialModel<DType, 1>;
 
 auto loss(const auto& model, const auto& x) {
     PROFILE_SCOPE("loss");
@@ -43,13 +63,15 @@ int main() {
     using Dim = Dimension<100>;
     using DType = float;
 
-    // auto x = zeros<DType, MakeShape<Dim>>();
-    auto x = arange<DType, Dim>() + randn<DType, MakeShape<Dim>>() * 0.1;
+    auto r = arange<DType, Dim>();
+    auto noise = randn_like(r);
 
-    LinearModel<DType> model;
+    auto x = 5 * pow(r, 2) + 3 * pow(r, 1) + 2 + 0.1 * noise;
+
+    PolynomialModel<DType, 2> model;
 
     const float lr = 0.1;
-    const int epochs = 1000;
+    const int epochs = 10'000;
 
     optim::Adam optimizer{lr, model.params()};
 
