08a4fa2aba6ac7e697232acf1c84115408fd272d
Alex Chen <axchen7@outlook.com>
2024-12-03 10:16:14 -0500

    Add benchmark demo

--- vgrad/cx_test.cpp
+++ vgrad/cx_test.cpp
@@ -1,31 +1,28 @@
 #include "complexity.h"
+#include "create_tensor.h"
+#include "ops.h"
 
 using namespace vgrad;
 using namespace vgrad::cx;
 
+template <Size N>
+void benchmark_matmul() {
+    using Dim = Dimension<N>;
+    auto mat = randn<float, MakeShape<Dim, Dim>>();
+    auto x = matmul(mat, mat);
+    x.mem_complexity.total();  // 🔍 [20120000 B] [160480000 B] [541080000 B] [1281920000 B] [2503000000 B] [4324320000 B] [6865880000 B] [10247680000 B] [14589720000 B] [20012000000 B]
+                               // B] [6865880000 B] [10247680000 B] [14589720000 B] [20012000000 B]
+}
+
 int main() {
-    using Dim1 = Dimension<10, "D">;
-    using Dim2 = Dimension<20, "E">;
-
-    using T1 = PolyTerm<Dim1, 2>;  // typehint: [D^2]
-    using T2 = PolyTerm<Dim2, 3>;  // typehint: [E^3]
-
-    using Const1 = Constant<100, "ns">;                      // typehint: [100 ns]
-    using ProdTerm1 = MakeProductTerm<T1, T2>;               // typehint: [D^2 x E^3]
-    using CProdTerm1 = ConstProductTerm<Const1, ProdTerm1>;  // typehint: [100 ns x D^2 x E^3]
-
-    using Const2 = Constant<200, "ns">;                      // typehint: [200 ns]
-    using ProdTerm2 = MakeProductTerm<T1, T2>;               // typehint: [D^2 x E^3]
-    using CProdTerm2 = ConstProductTerm<Const2, ProdTerm2>;  // typehint: [200 ns x D^2 x E^3]
-
-    using Const3 = Constant<50, "ns">;                       // typehint: [50 ns]
-    using ProdTerm3 = MakeProductTerm<T1>;                   // typehint: [D^2]
-    using CProdTerm3 = ConstProductTerm<Const3, ProdTerm3>;  // typehint: [50 ns x D^2]
-
-    using _RunningTime1 = MakeComplexity<CProdTerm1, CProdTerm2, CProdTerm3>;
-    using RunningTime1 = _RunningTime1;  // typehint: [50 ns x D^2 + 300 ns x D^2 x E^3]
-
-    using RunningTime2 = AddComplexities<RunningTime1, RunningTime1>;  // typehint: [100 ns x D^2 + 600 ns x D^2 x E^3]
-
-    using Foo = RunningTime2::TotalValue;  // typehint: [480010000 ns]
+    benchmark_matmul<100>();
+    benchmark_matmul<200>();
+    benchmark_matmul<300>();
+    benchmark_matmul<400>();
+    benchmark_matmul<500>();
+    benchmark_matmul<600>();
+    benchmark_matmul<700>();
+    benchmark_matmul<800>();
+    benchmark_matmul<900>();
+    benchmark_matmul<1000>();
 }
